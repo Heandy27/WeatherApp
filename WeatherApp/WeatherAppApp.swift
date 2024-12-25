301c696ccf8d3fdefaa0b0e7abc687c26cdd6c6a
Heandy27 <andy_heredia2008@hotmail.com>
2024-12-25 13:34:21 -0500

    Initial Commit

---
+++ WeatherApp/WeatherAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  WeatherAppApp.swift
+//  WeatherApp
+//
+//  Created by Andy Heredia on 25/12/24.
+//
+
+import SwiftUI
+
+@main
+struct WeatherAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
