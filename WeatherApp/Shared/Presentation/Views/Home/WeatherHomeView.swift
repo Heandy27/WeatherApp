5f4cdd5bfe6b98fba4cadb89aab131a1fdaaaa63
Heandy27 <andy_heredia2008@hotmail.com>
2024-12-25 17:18:05 -0500

    Pattern MVVM Added, Network Weather working with repository, usecase and UI Aswell

---
+++ WeatherApp/Shared/Presentation/Views/Home/WeatherHomeView.swift
@@ -0,0 +1,18 @@
+//
+//  WeatherHomeView.swift
+//  WeatherApp
+//
+//  Created by Andy Heredia on 25/12/24.
+//
+
+import SwiftUI
+
+struct WeatherHomeView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    WeatherHomeView()
+}
